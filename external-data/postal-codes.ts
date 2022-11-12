7f3a0656e360df0f71bdb0815cfb65ac1d887cf9
Pierrick Houée <houee.pierrick@gmail.com>
2022-11-12 12:11:14 +0100

    git bugs

--- external-data/postal-codes.ts
+++ external-data/postal-codes.ts
@@ -1,5 +1,23 @@
-const postOfficeData = require("./laposte_hexasmal.json");
+import data from "./laposte_hexasmal.json" assert { type: "json" };
 
+type GPSCoords = [number, number];
+
+interface postData {
+  datasetid: string;
+  recordid: string;
+  fields: {
+    nom_de_la_commune: string;
+    libelle_d_acheminement: string;
+    code_postal: string;
+    coordonnees_gps: GPSCoords;
+    code_commune_insee: string;
+  };
+  geometry: { type: string; coordinates: GPSCoords };
+  record_timestamp: string;
+}
+
+// const postOfficeData = require("./laposte_hexasmal.json");
+const postOfficeData: postData[] = data as postData[];
 let allPostalCodes: string[] = [];
 
 for (let i = 0; i < postOfficeData.length; i++) {
--- index.ts
+++ index.ts
@@ -1,16 +1,1 @@
-const pupeteer = require("pupeteer");
 
-const fs = require("fs");
-
-// Get list of pharmacies on a page (left)
-const getList = async () => {
-  const browser = await pupeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(
-    "https://www.ordre.pharmacien.fr/annuaire/etablissement?zipcode=92100&page=1",
-    { waitUntill: "networkidle2" }
-  );
-  return document.querySelectorAll(".picto-pharmacies");
-};
-
-console.log(getList());
